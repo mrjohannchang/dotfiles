6aa51c57d00e734ad666b35d23b527c6f40ad46a
Henry Chang <mr.changyuheng@gmail.com>
2021-07-04 11:19:03 +0800

    Update

--- scripts/ubuntu/swap-option-and-command-for-realforce-for-mac.sh
+++ scripts/ubuntu/swap-option-and-command-for-realforce-for-mac.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "${BASH_SOURCE[0]}" = /home/* ]]; then
+if [[ ~ = /home/* ]] || [[ "${BASH_SOURCE[0]}" = /home/* ]]; then
   username=$(echo "${BASH_SOURCE[0]}" | sed 's%^/home/\([^/]\+\)/.*%\1%g')
 else
   username=${USER}
 fi
-xenv="env DISPLAY=:0 XAUTHORITY=/run/user/$(id -u $username)/gdm/Xauthority"
+xenv="env DISPLAY=:0 XAUTHORITY=/home/${username}/.Xauthority"
 
 $xenv xinput -list | grep -F REALFORCE | while read -r line; do
   keyboard_id=$(echo $line | sed 's/.*id=\([0-9]\+\).*/\1/g')
