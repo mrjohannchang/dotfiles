f3e1db1ba22e2a2637c0a251215194848acb9d40
Johann Chang <johann.chang@outlook.com>
2022-08-06 18:11:40 +0800

    Fix a tab completion error in nvim-cmp

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -134,8 +134,16 @@ require "lualine".setup {
 
 
 -- nvim-cmp {
-local cmp = require'cmp'
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local cmp = require'cmp'
 cmp.setup({
   -- snippet = {
   --   -- REQUIRED - you must specify a snippet engine
