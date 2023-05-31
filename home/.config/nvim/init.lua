ea24c4c9b6f61b2296c87f9bdf122e3d1712ea83
Johann Chang <johann.chang@outlook.com>
2023-05-30 21:49:52 -0700

    Fix a nvim-cmp crash issue that's caused by no snippet engine

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -254,15 +254,15 @@ end
 
 local cmp = require("cmp")
 cmp.setup({
-  -- snippet = {
-  --   -- REQUIRED - you must specify a snippet engine
-  --   expand = function(args)
-  --     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-  --     -- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-  --     -- require("snippy").expand_snippet(args.body) -- For `snippy` users.
-  --     -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-  --   end,
-  -- },
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+      -- require("snippy").expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
   window = {
     -- completion = cmp.config.window.bordered(),
     -- documentation = cmp.config.window.bordered(),
