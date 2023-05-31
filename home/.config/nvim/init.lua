2c5b5f578a12bffaf930943cb39c12aaffec2b12
Johann Chang <johann.chang@outlook.com>
2023-05-30 18:26:28 -0700

    Enable support to installed LSP server automatically

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -217,18 +217,22 @@ vim.api.nvim_command("autocmd InsertLeave * set nopaste")
 -- mason.nvim {
 require("mason").setup()
 require("mason-nvim-dap").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "gopls",
-    "lua_ls",
-    "pylsp",
-    "rust_analyzer",
-  },
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers({
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function (server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {}
+  end,
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for the `rust_analyzer`:
+  -- ["rust_analyzer"] = function ()
+  --   require("rust-tools").setup {}
+  -- end
 })
 -- } mason.nvim
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").pylsp.setup {}
-require("lspconfig").rust_analyzer.setup {}
 -- } lspconfig
 
 
