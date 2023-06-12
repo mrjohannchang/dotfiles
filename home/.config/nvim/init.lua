6f356ec5d02c30218c4f480cc65a067e4b933cf6
Johann Chang <johann.chang@outlook.com>
2023-06-12 00:39:14 -0700

    Update the Neovim config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -324,7 +324,7 @@ require("lazy").setup({
 
 
 -- lspconfig {
--- mason.nvim {
+-- mason.nvim related {
 require("mason").setup()
 require("mason-nvim-dap").setup()
 require("mason-lspconfig").setup()
@@ -333,7 +333,7 @@ require("mason-lspconfig").setup_handlers({
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
-  function (server_name) -- default handler (optional)
+  function(server_name) -- default handler (optional)
     require("lspconfig")[server_name].setup {}
   end,
   -- Next, you can provide a dedicated handler for specific servers.
@@ -341,8 +341,9 @@ require("mason-lspconfig").setup_handlers({
   -- ["rust_analyzer"] = function ()
   --   require("rust-tools").setup {}
   -- end
+  ensure_installed = { "lua_ls" },
 })
--- } mason.nvim
+-- } mason.nvim related
 -- } lspconfig
 
 
