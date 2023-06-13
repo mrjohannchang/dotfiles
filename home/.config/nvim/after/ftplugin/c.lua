54fa46d602c29fd6e8b8bb0dadf5fa191ea20e61
Johann Chang <johann.chang@outlook.com>
2023-06-12 18:15:53 -0700

    Fix not formatting C/C++_files properly issue

--- home/.config/nvim/after/ftplugin/c.lua
+++ home/.config/nvim/after/ftplugin/c.lua
@@ -1,11 +1,11 @@
 if vim.fn.executable("clang-format") == 1 then
   if vim.fn.findfile(".clang-format", ".;") ~= "" then
     local c_auto_format_group = vim.api.nvim_create_augroup("CAutoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = { "*.c", ".h" },
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = { "*.c", "*.h" },
       callback = function()
         local p = vim.fn.getpos(".")
-        vim.cmd(':silent exec "%!clang-format"')
+        vim.cmd(":silent exec '!clang-format -i %'")
         vim.fn.setpos(".", p)
       end,
       group = c_auto_format_group,
