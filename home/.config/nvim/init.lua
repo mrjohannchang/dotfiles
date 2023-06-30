39cb1f23868e011246f043b99e451d3ab5980147
Johann Chang <johann.chang@outlook.com>
2023-06-30 20:34:45 +0800

    Update the default indent to 4 spaces

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -2,9 +2,9 @@
 vim.opt.cursorcolumn = true
 
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 
 vim.opt.fileformats = "unix,dos"
 
