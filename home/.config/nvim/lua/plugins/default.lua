42740ead7aa75c81945753a311d87e6f52d58723
Johann Chang <johann.chang@outlook.com>
2024-09-05 21:30:12 +0800

    Update Neovim colorscheme priority

--- home/.config/nvim/lua/plugins/default.lua
+++ home/.config/nvim/lua/plugins/default.lua
@@ -388,7 +388,6 @@ return {
       vim.cmd.colorscheme("NeoSolarized")
     end,
     lazy = false,  -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000,  -- make sure to load this before all the other start plugins
   },
 
   {
