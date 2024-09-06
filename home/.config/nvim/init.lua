a4464c5a38c50d0a838c3ad64f99d15e4932e7e1
Johann Chang <johann.chang@outlook.com>
2024-09-06 14:09:11 +0800

    Update nvim-treesitter config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -638,7 +638,16 @@ require("lazy").setup({
 
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":tsupdate",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
   },
 
   {
