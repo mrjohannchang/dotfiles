f34196908c919076e9cbb1253e51768e7662e8c8
Johann Chang <johann.chang@outlook.com>
2025-10-30 20:45:24 +0800

    feat: replace yapf with ruff for Python formatting

--- home/.config/nvim/after/ftplugin/python.lua
+++ home/.config/nvim/after/ftplugin/python.lua
@@ -2,19 +2,14 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 
-if vim.fn.executable("yapf") == 1 or vim.fn.executable("yapf3") == 1 then
-  if vim.fn.findfile(".style.yapf", ".;") ~= "" then
+if vim.fn.executable("ruff") == 1 then
+  if vim.fn.findfile("ruff.toml", ".;") ~= "" then
     local python_auto_format_group = vim.api.nvim_create_augroup("PythonAutoFormat", {})
     vim.api.nvim_create_autocmd("BufWritePost", {
       pattern = "*.py",
       callback = function()
         local p = vim.fn.getpos(".")
-        if vim.fn.executable("yapf") == 1 then
-          vim.cmd(':silent exec "%!yapf"')
-        end
-        if vim.fn.executable("yapf3") == 1 then
-          vim.cmd(':silent exec "%!yapf3"')
-        end
+        vim.cmd(":silent exec '!ruff format %'")
         vim.fn.setpos(".", p)
       end,
       group = python_auto_format_group,
